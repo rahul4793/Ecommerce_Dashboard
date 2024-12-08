a2682b4a6afdf5208d730583a45520146e4ef930
Rahul Gupta <73338505+rahul4793@users.noreply.github.com>
2024-12-08 15:35:38 +0530

    Update product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
@@ -8,5 +9,7 @@ const ProductSchema = new mongoose.Schema({
   dateOfSale: Date,
   sold: Boolean,
   category: String,
+  image: { type: String, required: false }, // Add image field (URL or path)
 });
-module.exports = mongoose.model('Product', ProductSchema);
+
+module.exports = mongoose.model('Productsnew', ProductSchema);
